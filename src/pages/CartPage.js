c6d6a6243e911ba3a2226a5ec314daab2d4d3c71
Madhurisha V <129486999+fruzie99@users.noreply.github.com>
2024-06-21 16:44:01 +0530

    Add files via upload

---
+++ src/pages/CartPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const CartPage = () => {
+  return (
+    <div>
+      <h2>Shopping Cart</h2>
+      <CartItems />
+      <button>Proceed to Checkout</button>
+    </div>
+  );
+};
+
+export default CartPage;
